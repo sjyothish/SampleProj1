5b45bfe1fe0dbb04b5460d20101f2ab3203fbbe2
Jyothish Babu <sjyothish@ssdi.sharp.co.in>
2019-10-30 12:13:05 +0530

    Removed unwanted folders

--- TestMessage/Program.cs
+++ TestMessage/Program.cs
@@ -10,7 +10,7 @@ namespace TestMessage
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Jyothish Babu");
+            System.Console.WriteLine("Success");
         }
     }
 }
